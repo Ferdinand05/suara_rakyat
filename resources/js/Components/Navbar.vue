45b5acc6a0c8077bcfe688d8ac52d3118897e74a
Ferdinand05 <fkoryanto@gmail.com>
2024-11-25 19:41:35 +0700

    create login and register view

--- resources/js/Components/Navbar.vue
+++ resources/js/Components/Navbar.vue
@@ -69,6 +69,20 @@
                         >
                     </li>
                     <li>
+                        <Link
+                            :href="route('login')"
+                            class="block py-2 px-3 text-white rounded hover:bg-gray-100/30 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                            >Login</Link
+                        >
+                    </li>
+                    <li>
+                        <Link
+                            :href="route('register')"
+                            class="block py-2 px-3 text-white rounded hover:bg-gray-100/30 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                            >Register</Link
+                        >
+                    </li>
+                    <li>
                         <button
                             id="dropdownNavbarLink"
                             data-dropdown-toggle="dropdownNavbar"
