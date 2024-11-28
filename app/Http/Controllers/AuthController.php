659f5f68b4e8db3f10d325e849ce78d8e7f9ae9d
Ferdinand05 <fkoryanto@gmail.com>
2024-11-28 12:45:11 +0700

    add feature : login and register, and middlware admin

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Providers\AppServiceProvider;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 use Inertia\Inertia;
 
 class AuthController extends Controller
@@ -12,12 +16,52 @@ class AuthController extends Controller
         return Inertia::render('Auth/LoginView');
     }
 
-    public function loginStore() {}
+    public function loginStore(Request $request)
+    {
+        $data = $request->validate([
+            'email' => ['required', 'email', 'exists:users,email'],
+            'password' => ['required']
+        ]);
+        if (Auth::attempt($data, $request->remember_me)) {
+            return to_route('/');
+        }
+
+        throw ValidationException::withMessages([
+            'email' => 'Kredensial tidak sesuai dengan yang terdaftar'
+        ]);
+
+
+        return redirect()->back();;
+    }
 
     public function registerIndex()
     {
         return Inertia::render('Auth/RegisterView');
     }
 
-    public function registerStore() {}
+    public function registerStore(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'min:3', 'max:100'],
+            'email' => ['required', 'unique:users,email'],
+            'no_telp' => ['required', 'max:15'],
+            'password' => ['required', 'min:7', 'confirmed']
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password,
+            'no_telp' => $request->no_telp,
+            'role_id' => 2
+        ]);
+
+        return redirect()->back();
+    }
+
+
+    public function logout()
+    {
+        Auth::logout();
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,16 +7,37 @@ use App\Http\Controllers\RoleController;
 use Illuminate\Container\Attributes\Auth;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\KategoriPengaduanController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', [HomeController::class, 'index'])->name('/');
 
 
-Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
-Route::resource('dashboard/role', RoleController::class);
-Route::resource('dashboard/kategori-pengaduan', KategoriPengaduanController::class);
 
-//SECTION - Authentication
-Route::get('login', [AuthController::class, 'loginIndex'])->name('login');
-Route::post('login', [AuthController::class, 'loginStore'])->name('login.store');
-Route::get('register', [AuthController::class, 'registerIndex'])->name('register');
-Route::post('register', [AuthController::class, 'registerStore'])->name('register.store');
+
+// SECTION LoggedIn
+Route::middleware('auth')->group(function () {
+
+
+    // Admin Only
+    Route::middleware('isAdmin')->group(function () {
+        Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+        Route::resource('dashboard/role', RoleController::class);
+        Route::resource('dashboard/kategori-pengaduan', KategoriPengaduanController::class);
+        Route::resource('dashboard/users', UserController::class);
+    });
+
+
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+});
+
+
+
+
+//SECTION Guest
+Route::middleware('guest')->group(function () {
+    //SECTION - Authentication
+    Route::get('login', [AuthController::class, 'loginIndex'])->name('login');
+    Route::post('login', [AuthController::class, 'loginStore'])->name('login.store');
+    Route::get('register', [AuthController::class, 'registerIndex'])->name('register');
+    Route::post('register', [AuthController::class, 'registerStore'])->name('register.store');
+});
