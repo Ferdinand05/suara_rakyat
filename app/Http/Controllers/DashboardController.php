653f908fc5666d455b9a8378280446f43d9fbc99
Ferdinand05 <fkoryanto@gmail.com>
2025-02-09 10:13:53 +0700

    addd charts

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\KategoriPengaduan;
+use App\Models\Pengaduan;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -10,6 +13,12 @@ class DashboardController extends Controller
     public function index()
     {
 
-        return Inertia::render('Dashboard/Index');
+        $counts = KategoriPengaduan::withCount('pengaduans')->orderBy('created_at')->get();
+        return Inertia::render('Dashboard/Index', [
+            'jumlahUser' => User::count(),
+            'jumlahPengaduan' => Pengaduan::count(),
+            'jumlahPengaduanPending' => Pengaduan::where('status', 'pending')->count(),
+            'kategoriPengaduanCount' => $counts
+        ]);
     }
 }
--- resources/js/Pages/Dashboard/Index.vue
+++ resources/js/Pages/Dashboard/Index.vue
@@ -3,26 +3,94 @@
         <div class="grid md:grid-cols-3 grid-cols-1 gap-3 md:gap-10 mb-6">
             <div class="bg-red-500 text-center md:py-8 py-5 rounded-md">
                 <div class="text-2xl font-semibold md:text-3xl">Users</div>
-                <div class="font-semibold text-xl md:text-2xl">10</div>
+                <div class="font-semibold text-xl md:text-2xl">
+                    {{ jumlahUser }}
+                </div>
             </div>
             <div class="bg-yellow-500 text-center md:py-8 py-5 rounded-md">
                 <div class="text-2xl font-semibold md:text-3xl">Pengaduan</div>
-                <div class="font-semibold text-xl md:text-2xl">10</div>
+                <div class="font-semibold text-xl md:text-2xl">
+                    {{ jumlahPengaduan }}
+                </div>
             </div>
             <div class="bg-green-500 text-center md:py-8 py-5 rounded-md">
                 <div class="text-2xl font-semibold md:text-3xl">
                     Pengaduan <span class="italic text-sm">Pending</span>
                 </div>
-                <div class="font-semibold text-xl md:text-2xl">10</div>
+                <div class="font-semibold text-xl md:text-2xl">
+                    {{ jumlahPengaduanPending }}
+                </div>
             </div>
         </div>
         <hr />
         <div class="my-3">
             <h3 class="font-semibold text-xl md:text-2xl">Statistik</h3>
+            <div>
+                <Bar
+                    id="my-chart-id"
+                    :options="chartOptions"
+                    :data="chartData"
+                />
+            </div>
+            <div>
+                <Doughnut :options="chartOptions" :data="chartData"></Doughnut>
+            </div>
         </div>
     </DashboardLayout>
 </template>
 
 <script setup>
+import { onMounted } from "vue";
 import DashboardLayout from "../Layouts/DashboardLayout.vue";
+defineProps({
+    jumlahUser: Number,
+    jumlahPengaduan: Number,
+    jumlahPengaduanPending: Number,
+    kategoriPengaduanCount: Object,
+});
+import { usePage } from "@inertiajs/vue3";
+const page = usePage();
+import { Bar } from "vue-chartjs";
+import {
+    Chart as ChartJS,
+    Title,
+    Tooltip,
+    Legend,
+    BarElement,
+    CategoryScale,
+    LinearScale,
+} from "chart.js";
+import { color } from "chart.js/helpers";
+
+ChartJS.register(
+    Title,
+    Tooltip,
+    Legend,
+    BarElement,
+    CategoryScale,
+    LinearScale
+);
+
+const kategoriPengaduan = page.props.kategoriPengaduanCount;
+const labelKategori = kategoriPengaduan.map((item) => {
+    return item.nama_kategori;
+});
+const countKategori = kategoriPengaduan.map((item) => {
+    return item.pengaduans_count;
+});
+
+const chartData = {
+    labels: labelKategori,
+    datasets: [
+        {
+            label: "Total",
+            backgroundColor: "lightblue",
+            data: countKategori,
+        },
+    ],
+};
+
+const chartOptions = {
+    responsive: true,
+};
 </script>
