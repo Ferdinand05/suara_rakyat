9ff2d979ada77180b476eec7b463f0938ada0d02
Ferdinand05 <fkoryanto@gmail.com>
2025-02-09 10:14:45 +0700

    add filter/search and cancel pengaduan

--- app/Http/Controllers/PengaduanController.php
+++ app/Http/Controllers/PengaduanController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\PengaduanResources;
+use App\Models\KategoriPengaduan;
 use App\Models\Pengaduan;
+use App\Models\Tanggapan;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -11,8 +13,24 @@ class PengaduanController extends Controller
 {
     public function index(Request $request)
     {
+
+        $keyword = $request->keyword;
+        $slug = $request->slug;
+
+        if ($slug && $slug !== 'semua') {
+            $kategori = KategoriPengaduan::where('slug', $request->slug)->first();
+            $pengaduan = Pengaduan::where('kategori_id', $kategori->id)->paginate(5)->withQueryString();
+        } else if ($keyword) {
+            $pengaduan = Pengaduan::where('status', 'like', "%$keyword%")->paginate(5)->withQueryString();
+        } else {
+            $pengaduan = Pengaduan::paginate(5)->withQueryString();
+        }
+
+
+
         return Inertia::render('Pengaduan/PengaduanView', [
-            'pengaduan' => PengaduanResources::collection(Pengaduan::all())
+            'pengaduan' => PengaduanResources::collection($pengaduan),
+            'kategoriPengaduan' => KategoriPengaduan::all()
         ]);
     }
 
@@ -26,5 +44,25 @@ class PengaduanController extends Controller
         Pengaduan::where('id', $request->id)->update([
             'status' => $request->status
         ]);
+    }
+
+    public function cancelPengaduan(Request $request, $id)
+    {
+
+        $pengaduan = Pengaduan::find($id);
+        if ($pengaduan) {
+            $tanggapan = Tanggapan::where('pengaduan_id', $pengaduan->id)->delete();
+            $pengaduan->delete();
+        }
+    }
+
+
+    public function destroy($id)
+    {
+        $pengaduan = Pengaduan::find($id);
+
+        if ($pengaduan) {
+            $pengaduan->delete();
+        }
     }
 }
